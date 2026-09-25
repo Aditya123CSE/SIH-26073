64f0454a3e7803620b236188fc92e6c10c3cec46
Aditya Sharma <adihunny123@gmail.com>
2026-09-26 01:12:23 +0530

    Fix ASGI application export in wsgi.py and your_application/wsgi.py for Gunicorn UvicornWorker

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,3 @@
 from backend_server import app
 
-try:
-    from a2wsgi import ASGIMiddleware
-    application = ASGIMiddleware(app)
-except Exception:
-    application = app
+application = app
--- your_application/wsgi.py
+++ your_application/wsgi.py
@@ -1,8 +1,3 @@
-import sys
 from backend_server import app
 
-try:
-    from a2wsgi import ASGIMiddleware
-    application = ASGIMiddleware(app)
-except Exception:
-    application = app
+application = app
